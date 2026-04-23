a5cce76fdfbeb4b9a9f7106baeb904de898ef4a5
aarshsidana <aarshsidana@gmail.com>
2026-04-23 20:48:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+
+#Load the ImDB dataset
+word_index = imdb.get_word_index()
+reversed_word_index = {value: key for key, value in word_index.items()}
+
+#load the trained model
+model=load_model('simplernn_imdb.h5')
+
+# step 2: Helper function to decode reviews
+def decode_review(encoded_review):
+    return ' '.join([reversed_word_index.get(i - 3, '?') for i in encoded_review])
+
+#Function to preprocess the review
+def preprocess_review(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word, 2) + 3 for word in words]
+    padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
+    return padded_review
+
+
+#Streamlit app
+import streamlit as st
+st.title("IMDB Movie Review Sentiment Analysis")
+st.write("Enter a movie review to predict its sentiment (positive or negative).")
+user_input = st.text_area("Movie Review", height=150)
+
+if st.button('Classify'):
+    processed_input = preprocess_review(user_input)
+
+    #Make prediction
+    prediction= model.predict(processed_input)[0][0]
+    sentiment = 'Positive' if prediction >= 0.5 else 'Negative'
+
+    st.write(f"Predicted Sentiment: {sentiment}")
+    st.write(f"Confidence: {prediction:.2f}")
+
+else:
+    st.write("Please enter a movie review and click 'Classify' to see the prediction.")
